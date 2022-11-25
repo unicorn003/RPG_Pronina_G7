8afe618d508a0dc4dc15e0ef9a1839275d8fd0e2
unicorn03 <valentyna.pronina@nure.ua>
2022-11-25 10:26:14 +0100

    I have added class Item

---
+++ src/main/java/com/isep/rpg/Item.java
@@ -0,0 +1,14 @@
+package com.isep.rpg;
+
+public abstract class Item {
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+}
