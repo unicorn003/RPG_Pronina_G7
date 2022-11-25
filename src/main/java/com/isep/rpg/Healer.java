db012e11eea16408854063904770592732ec3292
unicorn03 <valentyna.pronina@nure.ua>
2022-11-25 10:36:16 +0100

    I have added class Healer

---
+++ src/main/java/com/isep/rpg/Healer.java
@@ -0,0 +1,17 @@
+package com.isep.rpg;
+
+public class Healer extends SpellCaster{
+    public Healer(String n, int h) {
+        super(n, h);
+    }
+
+    @Override
+    public void fight(Combatant combatant) {
+
+    }
+
+    @Override
+    public void take(Item item) {
+
+    }
+}
