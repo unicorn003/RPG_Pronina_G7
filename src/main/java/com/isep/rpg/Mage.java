ddc1e3d87e6f9cdc7a468c01d619b18e98faf720
unicorn03 <valentyna.pronina@nure.ua>
2022-11-25 10:42:24 +0100

    I have added Input Parser and class Mage

---
+++ src/main/java/com/isep/rpg/Mage.java
@@ -0,0 +1,17 @@
+package com.isep.rpg;
+
+public class Mage extends SpellCaster{
+    public Mage(String n, int h) {
+        super(n, h);
+    }
+
+    @Override
+    public void fight(Combatant combatant) {
+
+    }
+
+    @Override
+    public void take(Item item) {
+
+    }
+}
