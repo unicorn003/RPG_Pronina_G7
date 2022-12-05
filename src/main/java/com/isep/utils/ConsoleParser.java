f28275eadb2fdeecea6991dfd75eb0e47421b834
unicorn03 <valentyna.pronina@nure.ua>
2022-12-05 12:37:56 +0100

    I am working on communication with user

--- src/main/java/com/isep/utils/ConsoleParser.java
+++ src/main/java/com/isep/utils/ConsoleParser.java
@@ -1,4 +1,27 @@
 package com.isep.utils;
 
+import com.isep.rpg.Combatant;
+
+import java.util.List;
+
 public class ConsoleParser implements InputParser{
+    @Override
+    public void askName() {
+
+    }
+
+    @Override
+    public void sayWelocome() {
+
+    }
+
+    @Override
+    public void startGame() {
+
+    }
+
+    @Override
+    public void showHeros(List<Combatant> heros) {
+
+    }
 }
--- src/main/java/com/isep/utils/InputParser.java
+++ src/main/java/com/isep/utils/InputParser.java
@@ -1,5 +1,14 @@
 package com.isep.utils;
 
+import com.isep.rpg.Combatant;
+
+import java.util.List;
+
 public interface InputParser {
-    
+    public void askName();
+    public void sayWelocome();
+    public void startGame();
+    public void showHeros(List<Combatant> heros);
+
+
 }
