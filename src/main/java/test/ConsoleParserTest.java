d7d7423e53467d8ab3f25d3fa8239095ef7e9e1b
unicorn03 <valentyna.pronina@nure.ua>
2022-12-14 22:36:31 +0100

    Added a tests

--- src/main/java/test/ConsoleParserTest.java
+++ src/main/java/test/ConsoleParserTest.java
@@ -1,12 +1,25 @@
 package test;
 
+import com.isep.rpg.Hunter;
+import com.isep.rpg.Warrior;
+import com.isep.utils.ConsoleParser;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConsoleParserTest {
 
+    Hunter natan = new Hunter("Natan");
+    Warrior sam = new Warrior("Sam");
+    ConsoleParser cP = new ConsoleParser();
     @Test
-    void getRewards() {
+    void getRewardsHunter() {
+        cP.getRewards(natan);
+        assertEquals(natan.getAttackPoint(),44);
+    }
+    @Test
+    void getRewardsWarrior(){
+        cP.getRewards(sam);
+        assertEquals(sam.getAttackPoint(), 48);
     }
 }
